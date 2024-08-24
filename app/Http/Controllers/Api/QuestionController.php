cf117c02d63205d6e97850b7a209577bb529687a
Eimantas Jucys <jucys.e@gmail.com>
2024-08-24 22:23:32 +0300

    caching example

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Collections\QuestionCollection;
 use App\Models\Question;
+use Illuminate\Support\Facades\Cache;
 
 class QuestionController extends Controller
 {
     public function getQuestions(): QuestionCollection
     {
-        $questions = Question::with('options')->get();
-        return new QuestionCollection($questions);
+        return Cache::remember('questions', 60, function () {
+            $questions = Question::with('options')->get();
+            return new QuestionCollection($questions);
+        });
     }
 }
